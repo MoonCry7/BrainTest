74bfe047a45caf0995c9a55fe0504c337aadd540
MoonCry7 <116568527+MoonCry7@users.noreply.github.com>
2022-10-24 16:04:53 +0300

    Add files via upload

---
+++ zadanie_1/zadanie_1.php
@@ -0,0 +1,20 @@
+<?php
+$articleLink = 'https://habr.com/ru/post/44098/'; //ссылка на статью
+$articleText = file_get_contents('article.txt'); //читаем статью из файла
+$aclean = preg_replace('/[\x00-\x1F\x7F]/u', '', $articleText); //убираем лишние символы
+$articlePreview = mb_substr($aclean, 0, 200)."..."; //обрезаем текст статьи до 200 символов и добавл€ем в конце многоточие
+
+$words = explode(' ',$articlePreview); //разбирваем строку
+//ƒалее делаем многоточие и три последних слова гиперссылкой на статью
+$co = count($words);
+    foreach ($words as $key => $word) {
+    if($key == $co-3)
+    echo '<a href="'.$articleLink.'">';
+    if($key == $co-1)
+    echo $word;
+    else
+    echo $word.' ';
+    if($key == $co-1)
+    echo '<a>';
+    }
+?>
